6694b40f1c1634744c607a0e4a9431685f250ab0
gopalpatil0318 <gopalpatilrcpit@gmail.com>
2024-12-28 22:59:44 +0530

    razor pay added

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -4,7 +4,6 @@ import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import BottomNavigation from '@/components/BottomNavigation';
 import { useSession } from 'next-auth/react';
-import DummyPayment from '@/components/DummyPayment';
 
 interface PaymentDetails {
     paymentId: string;
@@ -20,19 +19,14 @@ interface CartItem {
 export default function Checkout() {
     const [mobileNumber, setMobileNumber] = useState('');
     const [address, setAddress] = useState('');
-    const [showPayment, setShowPayment] = useState(false);
     const router = useRouter();
     const { status } = useSession();
 
-    const handleNext = () => {
-        if (!mobileNumber || !address) {
-            alert('Please fill in all fields');
-            return;
-        }
-        setShowPayment(true);
+    const initiatePayment = () => {
+        window.location.href = "trigger-razorpay-payment";
     };
 
-    const handlePaymentSuccess = async (paymentDetails: PaymentDetails) => {
+    const handlePaymentSuccess = async (paymentId: string) => {
         try {
             const cartResponse = await fetch('/api/cart');
             const cartItems: CartItem[] = await cartResponse.json();
@@ -46,7 +40,7 @@ export default function Checkout() {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
-                    paymentId: paymentDetails.paymentId,
+                    paymentId,
                     address,
                     mobileNumber,
                     items: cartItems,
@@ -66,6 +60,18 @@ export default function Checkout() {
         }
     };
 
+    const handlePaymentFailure = (error: string) => {
+        alert("Payment Failed: " + error);
+    };
+
+    const handleNext = () => {
+        if (!mobileNumber || !address) {
+            alert('Please fill in all fields');
+            return;
+        }
+        initiatePayment();
+    };
+
     if (status === 'loading') {
         return <div>Loading...</div>;
     }
@@ -82,57 +88,54 @@ export default function Checkout() {
             </div>
 
             <div className="p-4">
-                {!showPayment ? (
-                    <div className="mt-10">
-                        <form onSubmit={(e) => { e.preventDefault(); handleNext(); }} className="space-y-2">
-                            <div>
-                                <label htmlFor="mobileNumber" className="block text-sm/6 w-full font-medium text-gray-900">
-                                    Mobile Number
-                                </label>
-                                <div className="mt-2">
-                                    <input
-                                        id="mobileNumber"
-                                        name="mobileNumber"
-                                        type="tel"
-                                        required
-                                        value={mobileNumber}
-                                        onChange={(e) => setMobileNumber(e.target.value)}
-                                        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                                    />
-                                </div>
+                <div className="mt-10">
+                    <form onSubmit={(e) => { e.preventDefault(); handleNext(); }} className="space-y-2">
+                        <div>
+                            <label htmlFor="mobileNumber" className="block text-sm/6 w-full font-medium text-gray-900">
+                                Mobile Number
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    id="mobileNumber"
+                                    name="mobileNumber"
+                                    type="tel"
+                                    required
+                                    value={mobileNumber}
+                                    onChange={(e) => setMobileNumber(e.target.value)}
+                                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                />
                             </div>
+                        </div>
 
-                            <div>
-                                <label htmlFor="address" className="block text-sm/6 font-medium text-gray-900">
-                                    Address
-                                </label>
-                                <div className="mt-2">
-                                    <textarea
-                                        id="address"
-                                        name="address"
-                                        required
-                                        value={address}
-                                        onChange={(e) => setAddress(e.target.value)}
-                                        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                                    ></textarea>
-                                </div>
+                        <div>
+                            <label htmlFor="address" className="block text-sm/6 font-medium text-gray-900">
+                                Address
+                            </label>
+                            <div className="mt-2">
+                                <textarea
+                                    id="address"
+                                    name="address"
+                                    required
+                                    value={address}
+                                    onChange={(e) => setAddress(e.target.value)}
+                                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                ></textarea>
                             </div>
+                        </div>
 
-                            <div>
-                                <button
-                                    type="submit"
-                                    className="flex mt-12 w-full justify-center rounded-md bg-[#009999] px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-[#006666] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                                >
-                                    Next
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                ) : (
-                    <DummyPayment onSuccess={handlePaymentSuccess} />
-                )}
+                        <div>
+                            <button
+                                type="submit"
+                                className="flex mt-12 w-full justify-center rounded-md bg-[#009999] px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-[#006666] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                                Checkout
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
             <BottomNavigation />
         </>
     );
 }
+
